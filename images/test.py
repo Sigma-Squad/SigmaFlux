e650be23156b02f769eae9d4737f633eb826f98f
Mactavish-star <121548326+Mactavish-star@users.noreply.github.com>
2025-04-05 18:01:11 +0530

    Update test.py

--- images/test.py
+++ images/test.py
@@ -1,1 +1,10 @@
 import numpy
+import os
+from shutil import move as move_file
+
+source_file = '/input/001.jpg'
+os.makedirs('processed', exist_ok=True)
+destination_dir = '/processed'
+
+# Move the file
+shutil.move(source_file, destination_dir)
