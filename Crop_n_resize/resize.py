c9d8de1c61aee655af1fcaf1b36ceee75c990a05
Srimadhav2007 <phanisrimadhav.mula@gmail.com>
2025-06-11 14:18:59 +0530

    Changed Crop_n_resize part

--- Crop_n_resize/resize.py
+++ Crop_n_resize/resize.py
@@ -2,7 +2,7 @@ import cv2
 
 #if the input is an image
 def resize(image):
-    return cv2.resize(image,(2013,1635),interpolation=cv2.INTER_LANCZOS4)
+    return cv2.resize(image,(2266,1841),interpolation=cv2.INTER_LANCZOS4)
 
 #if the imput is string containing the image location
 def resize_image_string(image_string):
