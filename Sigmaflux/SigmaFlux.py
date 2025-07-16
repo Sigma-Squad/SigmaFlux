7bb79cfe80b1a7bde9985409bdb5f0a0bb4d0e84
Shivnavesh <me24b044@iittp.ac.in>
2025-07-16 16:36:24 +0530

    Add files via upload

---
+++ Sigmaflux/SigmaFlux.py
@@ -0,0 +1,26 @@
+# main code
+
+import os
+import pre_processing_pipeline as pre_p         # importing preprocessing part
+import ocr_code as ocr                          # importing the ocr part
+import post_processing as post_p                # importing postprocessing part
+
+def main():
+    input_path = r"C:\Users\shivn\Downloads\pipeline\025.jpg"       # change the input path
+
+    n = int(input("Enter dates: "))                                 # Input for number of date columns
+
+    base, ext = os.path.splitext(input_path)
+
+    output_path = f"{base}_proc{ext}"                               # Output path for preprocessed images
+    
+    pre_p.run_parallel_image_processing(input_path, output_path)    # Calling pre_processing
+    ocr.ocr_reading(output_path,n)                                  # Calling llm model
+                                         
+    base, ext = os.path.splitext(output_path) 
+    new_output_path = f"{base}.txt"
+
+    post_p.excel_creation(new_output_path,n)                        # Calling post_processing
+
+if __name__ == "__main__":
+    main()
