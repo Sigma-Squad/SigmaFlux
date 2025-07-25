ce821cee5b8f8110e2fb67bd65bd046b9781457c
Aditya Kumar Gupta <rguptaaditya242011@gmail.com>
2025-07-25 19:40:18 +0530

    Update SigmaFlux.py

--- SigmaFlux.py
+++ SigmaFlux.py
@@ -4,11 +4,11 @@ import pre_processing_pipeline as pre_p         # importing preprocessing part
 import llm_code as ocr                          # importing the ocr part
 import post_processing as post_p                # importing postprocessing part
 
-def main(input_image,file_name,n):
+def main(input_image,file_name,n,api_key):
 
     
     output_image = pre_p.run_parallel_image_processing(input_image)
-    output_text = ocr.ocr_reading(output_image,n,file_name)
+    output_text = ocr.ocr_reading(output_image,n,file_name,api_key)
 
     df = post_p.excel_creation(output_text,n)
 
